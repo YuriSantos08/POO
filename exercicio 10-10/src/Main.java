c0cda045959fd04852aecb268bd659500b8f50bf
YuriSantos08 <yuridacosta25@gmail.com>
2025-10-12 01:37:43 -0300

    subindo atividade

--- exercicio 10-10/src/Main.java
+++ exercicio 10-10/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Animal> animais = new ArrayList<>();
+
+        animais.add(new Cachorro("Pluto"));
+        animais.add(new Cachorro("Rio"));
+        animais.add(new Gato("Garfield"));
+        animais.add(new Gato("Tom"));
+
+        for (Animal animal : animais) {
+            animal.exibirDetalhes();
+        }
     }
 }
